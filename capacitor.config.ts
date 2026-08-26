dc54924c33f177359ef5df79ae4735d6a0f7e31c
Harsh Khandelwal <harsh.khandelwal01@nagarro.com>
2026-08-26 10:48:18 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'share-trading',
+  webDir: 'www'
+};
+
+export default config;
